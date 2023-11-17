e531bcff7acbbb8109bc2d1474edecdab3b9f328
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2023-11-17 15:51:36 +0530

    non responsive ui

---
+++ web/script.js
@@ -0,0 +1,5 @@
+
+    // Set the height of the scrollbar thumb dynamically
+    const customScrollbar = document.getElementById('customScrollbar');
+    const thumbHeight = 4; // Set the desired height in vh
+    customScrollbar.style.paddingRight = `calc(${thumbHeight}vh - 8px)`; // Adjust the calculation based on your preferences
